442c6af232ec665d98ab45c99a9457c0e2fb1ecc
VirLg <19virlg@gmail.com>
2024-09-03 13:39:30 +0300

    main page descktop

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,42 +1,42 @@
 import React from "react";
 import bg from "../../images/svg/bg_main_deck.svg";
+import boolb_mp from "../../images/svg/boolb_mp.svg";
 import mp_right from "../../images/svg/mp_right.svg";
 import mp_left_one from "../../images/svg/mp_left_one.svg";
 import mp_left_two from "../../images/svg/mp_left_two.svg";
 const MainPage = () => {
   return (
-    <div className="container">
+    <div className="container mt-[49px]">
       {/* justify-between */}
       <div
-        className="flex justify-between ml-auto mr-auto w-[1014px] h-[582px] "
+        className="flex  ml-auto mr-auto w-[100%] h-[660px]  "
         style={{
-          backgroundImage: `url(${bg})`,
+          backgroundImage: `url(${bg}),url(${boolb_mp}`,
           backgroundSize: "contain",
           backgroundRepeat: "no-repeat",
           backgroundPosition: "0% 100%",
         }}>
-        <div className="w-[100%] h-[100%]">
+        <div className=" mt-[31px] pl-[198px]">
           <div
-            className="pt-[80px] h-[90px]"
+            className="w-[439px] h-[90px]"
             style={{
               backgroundImage: `url(${mp_left_one})`,
               backgroundRepeat: "no-repeat",
-              backgroundPosition: "10% 10%",
             }}></div>
           <div
-            className="pt-[80px]  w-[100%] h-[100%]"
+            className=" w-[439px] h-[184px] mt-[24px] mb-[24px]"
             style={{
               backgroundImage: `url(${mp_left_two})`,
               backgroundRepeat: "no-repeat",
-              backgroundPosition: "10% 10%",
             }}></div>
+          <button className="button_one">Try tracker</button>
         </div>
         <div
-          className="pt-[80px] w-[100%] h-[100%]"
+          className=" w-[494px] h-[332px] mt-[65px] ml-[81px]"
           style={{
             backgroundImage: `url(${mp_right})`,
             backgroundRepeat: "no-repeat",
-            backgroundPosition: "70% 10%",
+            backgroundSize: "contain",
           }}></div>
       </div>
     </div>
